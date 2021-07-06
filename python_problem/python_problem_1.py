ec644d6e8f4d4074fb188aa1c07209f6463b339d
Yeram <ramye@naver.com>
2021-07-07 02:09:42 +0900

    1-5

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -9,7 +9,13 @@ while True:
     except TypeError:
         print('1, 2, 3만 입력 가능')    
 
-i = 1
-while i <= num:
-    print('playerA : %d' %i)
-    i += 1
+current_num = 1
+while current_num <= num:
+    print('playerA : %d' %current_num)
+    current_num += 1
+
+B_tell_num = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :'))
+numB = current_num + B_tell_num
+while current_num < numB:
+    print('playerB : %d' %current_num)
+    current_num += 1
