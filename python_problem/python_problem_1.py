47365548e3eb9e84b465fc6988bf062eaad44280
Yeram <ramye@naver.com>
2021-07-07 03:48:50 +0900

    1-6

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,33 +1,42 @@
-
-
-while True:
-    try:
-        num = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :'))
-        if num >=4:
-            raise TypeError
-        break
-    except ValueError:
-        print('정수를 입력하세요')
-    except TypeError:
-        print('1, 2, 3만 입력 가능')    
-
 current_num = 1
-for current_num in range(1, num + 1):
-    print('playerA : %d' %current_num)
-    current_num += 1
-
-while True:
-    try:
-        B_tell_num = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :'))
-        if B_tell_num >=4:
-            raise TypeError
+numA = 0
+numB = 0
+num = 0
+while current_num < 31:
+    if current_num > 31:
         break
-    except ValueError:
-        print('정수를 입력하세요')
-    except TypeError:
-        print('1, 2, 3만 입력 가능')
-        
-numB = current_num + B_tell_num
-for current_num in range(current_num, numB):
-    print('playerB : %d' %current_num)
-    current_num += 1
+    else:
+        while True:
+            try:
+                num = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :'))
+                if num >=4:
+                    raise TypeError
+                break
+            except ValueError:
+                print('정수를 입력하세요')
+            except TypeError:
+                print('1, 2, 3만 입력 가능')    
+
+        numA = current_num + num
+        for current_num in range(current_num, numA):
+            print(f'playerA : {current_num}')
+            current_num += 1
+    
+    if current_num > 31:
+        break
+    else:
+        while True:
+            try:
+                num = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :'))
+                if num >=4:
+                    raise TypeError
+                break
+            except ValueError:
+                print('정수를 입력하세요')
+            except TypeError:
+                print('1, 2, 3만 입력 가능')
+
+        numB = current_num + num
+        for current_num in range(current_num, numB):
+            print(f'playerB : {current_num}')
+            current_num += 1
