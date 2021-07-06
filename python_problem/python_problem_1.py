b9427397b7f2c4c110cf2d041d3bba1d4cc89b73
Yeram <ramye@naver.com>
2021-07-07 01:46:10 +0900

    1-3

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,2 +1,10 @@
-num = 0
-tell_num = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :'))
+while True:
+    try:
+        num = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :'))
+        if num >=4:
+            raise TypeError
+        break
+    except ValueError:
+        print('정수를 입력하세요')
+    except TypeError:
+        print('1, 2, 3만 입력 가능')    
