ad9136dece56735ef86a938fa4835aec4d0a7530
Yeram <ramye@naver.com>
2021-07-06 21:41:16 +0900

    ch38

---
+++ 0703 파이썬 과제/ch38/error.py
@@ -0,0 +1,9 @@
+y=[10, 20, 30]
+
+try:
+    index, x = map(int, input('인덱스와 나눌 숫자를 입력하세요: ').split(' '))
+    print(y[index]/x)
+except ZeroDivisionError as e:
+    print('숫자를 0으로 나눌 수 없다', e)
+except IndexError as e:
+    print('잘못된 인덱스입니다.', e)
