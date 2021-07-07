62f46543d1676598f95ca134e4ab6cd503019215
Yeram <ramye@naver.com>
2021-07-08 04:21:03 +0900

    1-9

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,3 +1,4 @@
+import random
 current_num = 1
 numA = 0
 numB = 0
@@ -25,36 +26,25 @@ class brGame():
                     print('정수를 입력하세요')
                 except TypeError:
                     print('1, 2, 3만 입력 가능')    
-
             numA = current_num + num
             for current_num in range(current_num, numA):
                 print(f'playerA : {current_num}')
                 current_num += 1
 
-    def Bplayer(self):
+    def Computer(self):
         global current_num
         if current_num > 31:
-            print('playerB win!')
+            print('Computer win!')
             exit()
         else:
-            while True:
-                try:
-                    num = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :'))
-                    if num >=4:
-                        raise TypeError
-                    break
-                except ValueError:
-                    print('정수를 입력하세요')
-                except TypeError:
-                    print('1, 2, 3만 입력 가능')
-
+            num = random.randint(1, 3)
             numB = current_num + num
             for current_num in range(current_num, numB):
-                print(f'playerB : {current_num}')
-                current_num += 1        
+                print(f'Computer : {current_num}')
+                current_num += 1   
         
 
-brGame("Aplayer", "Bplayer")
+brGame("Aplayer", "Computer")
 while True:
     brGame.Aplayer("Aplayer")
-    brGame.Bplayer("Bplayer")
+    brGame.Computer("Computer")
