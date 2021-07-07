6df013af5d75513821878a921a9a8bfe19a57592
Yeram <ramye@naver.com>
2021-07-08 04:13:33 +0900

    1-8

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -3,43 +3,58 @@ numA = 0
 numB = 0
 num = 0
 
-while current_num <= 32:
-    if current_num > 31:
-        print('playerA win!')
-        break
-    else:
-        while True:
-            try:
-                num = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :'))
-                if num >=4:
-                    raise TypeError
-                break
-            except ValueError:
-                print('정수를 입력하세요')
-            except TypeError:
-                print('1, 2, 3만 입력 가능')    
+class brGame():
 
-        numA = current_num + num
-        for current_num in range(current_num, numA):
-            print(f'playerA : {current_num}')
-            current_num += 1
-    
-    if current_num > 31:
-        print('playerB win!')
-        break
-    else:
-        while True:
-            try:
-                num = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :'))
-                if num >=4:
-                    raise TypeError
-                break
-            except ValueError:
-                print('정수를 입력하세요')
-            except TypeError:
-                print('1, 2, 3만 입력 가능')
+    def __init__(self, Aplayer, Bplayer):
+        self.Aplayer = Aplayer
+        self.Bplayer = Bplayer
 
-        numB = current_num + num
-        for current_num in range(current_num, numB):
-            print(f'playerB : {current_num}')
-            current_num += 1
+    def Aplayer(self):
+        global current_num
+        if current_num > 31:
+            print('playerA win!')
+            exit()
+        else:
+            while True:
+                try:
+                    num = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :'))
+                    if num >=4:
+                        raise TypeError
+                    break
+                except ValueError:
+                    print('정수를 입력하세요')
+                except TypeError:
+                    print('1, 2, 3만 입력 가능')    
+
+            numA = current_num + num
+            for current_num in range(current_num, numA):
+                print(f'playerA : {current_num}')
+                current_num += 1
+
+    def Bplayer(self):
+        global current_num
+        if current_num > 31:
+            print('playerB win!')
+            exit()
+        else:
+            while True:
+                try:
+                    num = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :'))
+                    if num >=4:
+                        raise TypeError
+                    break
+                except ValueError:
+                    print('정수를 입력하세요')
+                except TypeError:
+                    print('1, 2, 3만 입력 가능')
+
+            numB = current_num + num
+            for current_num in range(current_num, numB):
+                print(f'playerB : {current_num}')
+                current_num += 1        
+        
+
+brGame("Aplayer", "Bplayer")
+while True:
+    brGame.Aplayer("Aplayer")
+    brGame.Bplayer("Bplayer")
--- python_problem/save.py
+++ python_problem/save.py
@@ -3,47 +3,62 @@ current_num = 1
 numA = 0
 numB = 0
 num = 0
-your_num = 0
-who = 0
-A = 0
-B = 0
 
-def brGame(your_num, current_num, who):
-    your_num = current_num + num
-    for current_num in range(current_num, your_num):
-        print(f'player{who} : {current_num}')
-        current_num += 1
+class brGame():
+
+    def __init__(self, Aplayer, Bplayer):
+        self.Aplayer = Aplayer
+        self.Bplayer = Bplayer
+
+    def Aplayer(self):
+        global current_num
+        if current_num > 31:
+            print('playerA win!')
+            exit()
+        else:
+            while True:
+                try:
+                    num = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :'))
+                    if num >=4:
+                        raise TypeError
+                    break
+                except ValueError:
+                    print('정수를 입력하세요')
+                except TypeError:
+                    print('1, 2, 3만 입력 가능')    
+
+            numA = current_num + num
+            for current_num in range(current_num, numA):
+                print(f'playerA : {current_num}')
+                current_num += 1
+
+    def Bplayer(self):
+        global current_num
+        if current_num > 31:
+            print('playerB win!')
+            exit()
+        else:
+            while True:
+                try:
+                    num = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :'))
+                    if num >=4:
+                        raise TypeError
+                    break
+                except ValueError:
+                    print('정수를 입력하세요')
+                except TypeError:
+                    print('1, 2, 3만 입력 가능')
+
+            numB = current_num + num
+            for current_num in range(current_num, numB):
+                print(f'playerB : {current_num}')
+                current_num += 1        
+        
+
+brGame("Aplayer", "Bplayer")
+while True:
+    brGame.Aplayer("Aplayer")
+    brGame.Bplayer("Bplayer")
 
 
-while current_num <= 32:
-    if current_num > 31:
-        print('playerA win!')
-        break
-    else:
-        while True:
-            try:
-                num = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :'))
-                if num >=4:
-                    raise TypeError
-                break
-            except ValueError:
-                print('정수를 입력하세요')
-            except TypeError:
-                print('1, 2, 3만 입력 가능')    
-        brGame(numA, current_num, A)
-    
-    if current_num > 31:
-        print('playerB win!')
-        break
-    else:
-        while True:
-            try:
-                num = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :'))
-                if num >=4:
-                    raise TypeError
-                break
-            except ValueError:
-                print('정수를 입력하세요')
-            except TypeError:
-                print('1, 2, 3만 입력 가능')
-        brGame(numB, current_num, B)
+
