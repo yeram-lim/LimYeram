56b65c01f981c2a13dcc02cf45e7762b26968326
Yeram <ramye@naver.com>
2021-07-07 16:35:24 +0900

    2-menu1 예외처리

--- python_problem/save.py
+++ python_problem/save.py
@@ -1,18 +1,8 @@
+#6단계까지 저장본
 current_num = 1
 numA = 0
 numB = 0
 num = 0
-your_num = 0
-who = 0
-A = 0
-B = 0
-
-def brGame(your_num, current_num, who):
-    your_num = current_num + num
-    for current_num in range(current_num, your_num):
-        print(f'player{who} : {current_num}')
-        current_num += 1
-
 
 while current_num <= 32:
     if current_num > 31:
@@ -29,7 +19,11 @@ while current_num <= 32:
                 print('정수를 입력하세요')
             except TypeError:
                 print('1, 2, 3만 입력 가능')    
-        brGame(numA, current_num, A)
+
+        numA = current_num + num
+        for current_num in range(current_num, numA):
+            print(f'playerA : {current_num}')
+            current_num += 1
     
     if current_num > 31:
         print('playerB win!')
@@ -45,4 +39,8 @@ while current_num <= 32:
                 print('정수를 입력하세요')
             except TypeError:
                 print('1, 2, 3만 입력 가능')
-        brGame(numB, current_num, B)
+
+        numB = current_num + num
+        for current_num in range(current_num, numB):
+            print(f'playerB : {current_num}')
+            current_num += 1
---
+++ python_problem/test.py
@@ -0,0 +1,4 @@
+mid, final = input('Enter name mid-score final-score : ').split()
+#print(str(type(int(mid))) != "<class 'int'>")
+print(str.isdigit(mid) == False)
+print(int(final))
