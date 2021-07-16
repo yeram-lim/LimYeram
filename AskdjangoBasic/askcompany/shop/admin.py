496c9d62bdfd0d9e22ac7bb207fd7adb4c9d8e50
Yeram <ramye@naver.com>
2021-07-16 17:03:13 +0900

    12강

--- AskdjangoBasic/askcompany/shop/admin.py
+++ AskdjangoBasic/askcompany/shop/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Item
 
-# Register your models here.
+#admin.site.register(Item) #admin사이트에 Item 저장하는 방법
+
+@admin.register(Item) #다른 등록 방법
+class ItemAdmin(admin.ModelAdmin): 
+    list_display = ['pk', 'name', 'short_desc', 'price', 'is_publish']
+    list_display_links = ['name']
+    list_filter = ['is_publish', 'updated_at']
+    search_fields = ['name']
+
+    def short_desc(self, item):
+        return item.desc[:20]
--- AskdjangoBasic/askcompany/shop/models.py
+++ AskdjangoBasic/askcompany/shop/models.py
@@ -4,5 +4,9 @@ class Item(models.Model):
     name = models.CharField(max_length=100)
     desc = models.TextField(blank=True) #빈칸 허용
     price = models.PositiveIntegerField()
+    is_publish = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'<{self.pk}> {self.name}'
