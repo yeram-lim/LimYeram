617fe2a739367a9dc5090c94bb4f74591d5126ce
Yeram <ramye@naver.com>
2021-07-16 16:25:13 +0900

    11강

--- AskdjangoBasic/askcompany/blog/models.py
+++ AskdjangoBasic/askcompany/blog/models.py
@@ -1,24 +1,12 @@
-from django.conf import settings
 from django.db import models
-class Profile(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    blog_url = models.URLField(blank=True)
+
 class Post(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    title = models.CharField(max_length=100, db_index=True)
-    slug = models.SlugField(allow_unicode=True, db_index=True) # ModelAdmin.prepopulated_fields 편리
-    desc = models.TextField(blank=True)
-    image = models.ImageField(blank=True) # Pillow 설치가 필요
-    comment_count = models.PositiveIntegerField(default=0)
-    tag_set = models.ManyToManyField('Tag', blank=True)
-    is_publish = models.BooleanField(default=False)
+    author_name = models.CharField(max_length=20)
+    title = models.CharField(max_length=100)
+    content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
 class Comment(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)#on_delete=models.CASCADE: 소속된 post가 삭제되면 comment도 삭제된다.
     message = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-class Tag(models.Model):
-    name = models.CharField(max_length=50, unique=True)
