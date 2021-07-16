b257d997bbe7661be7c2fe32f4f344c4cccee214
Yeram <ramye@naver.com>
2021-07-16 12:14:24 +0900

    09강

--- AskdjangoBasic/askcompany/shop/models.py
+++ AskdjangoBasic/askcompany/shop/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Item(models.Model):
+    name = models.CharField(max_length=100)
+    desc = models.TextField(blank=True) #빈칸 허용
+    price = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
