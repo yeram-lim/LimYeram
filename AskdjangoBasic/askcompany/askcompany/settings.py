6cd5a9fe588efee4f99889e836db9f10cc3e438c
Yeram <ramye@naver.com>
2021-07-18 17:36:20 +0900

    23-25강 Static 파일을 다루는 방법

--- AskdjangoBasic/askcompany/askcompany/settings.py
+++ AskdjangoBasic/askcompany/askcompany/settings.py
@@ -128,5 +128,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'askcompany', 'static'),
+]
 
 INTERNAL_IPS = ['127.0.0.1']
---
+++ AskdjangoBasic/askcompany/askcompany/static/main.js
@@ -0,0 +1,4 @@
+
+console.log("loaded askcompany/static/main.js");
+
+
