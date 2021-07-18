712489b71e3cef950435c0f8a3a06e9dfd2527da
Yeram <ramye@naver.com>
2021-07-18 20:30:11 +0900

    29강 장고 Form을 쓰지 않고, 글 생성/수정 구현하기

--- AskdjangoBasic/askcompany/shop/views.py
+++ AskdjangoBasic/askcompany/shop/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
-from django.shortcuts import get_object_or_404, render
+import re
+from django.shortcuts import get_object_or_404, redirect, render
 from .models import Item
 
 def archives_year(requset, year):
@@ -22,4 +23,70 @@ def item_detail(request, pk):
         'item': item,
     })
 
- 
+def item_new(request, item=None):
+    error_list = []
+    initial = {}
+
+    if request.method == 'POST':
+        data = request.POST
+        files = request.FILES
+
+        name = data.get('name')
+        desc = data.get('desc')
+        price = data.get('price')
+        photo = files.get('photo')
+        is_publish = data.get('is_publish') in (True, 't', 'True', '1')
+
+        # 유효성 검사
+        if len(name) < 2:
+            error_list.append('name을 2글자 이상 입력해주세요.')
+
+        if re.match(r'^[\da-zA-Z\s]+$', desc):
+            error_list.append('한글을 입력해주세요.')
+
+        if not error_list:
+            # 저장 시도
+            if item is None:
+                item = Item()
+
+            item.name = name
+            item.desc = desc
+            item.price = price
+            item.is_publish = is_publish
+
+            if photo:
+                item.photo.save(photo.name, photo, save=False)
+
+            try:
+                item.save()
+            except Exception as e:
+                error_list.append(e)
+            else:
+                return redirect(item)
+
+        initial = {
+            'name': name,
+            'desc': desc,
+            'price': price,
+            'photo': photo,
+            'is_publish': is_publish,
+        }
+    else:
+        if item is not None:
+            initial = {
+                'name': item.name,
+                'desc': item.desc,
+                'price': item.price,
+                'photo': item.photo,
+                'is_publish': item.is_publish,
+            }
+
+    return render(request, 'shop/item_form.html', {
+        'error_list': error_list,
+        'initial': initial,
+    })
+
+
+def item_edit(request, pk):
+    item = get_object_or_404(Item, pk=pk)
+    return item_new(request, item)
