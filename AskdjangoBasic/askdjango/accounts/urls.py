e116e881c7c79263c5db52319af4560159d610ca
Yeram <ramye@naver.com>
2021-07-19 13:03:14 +0900

    중급 03강 프로필 페이지 구현하기

--- AskdjangoBasic/askdjango/accounts/urls.py
+++ AskdjangoBasic/askdjango/accounts/urls.py
@@ -1,8 +1,10 @@
 from django.contrib.auth import views as auth_views
 from django.urls import path
+from . import views
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('profile/', views.profile, name='profile'),
 ]
--- AskdjangoBasic/askdjango/accounts/views.py
+++ AskdjangoBasic/askdjango/accounts/views.py
@@ -1,3 +1,6 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-# Create your views here.
+@login_required #로그인 안 한 사람은 settings의 login으로 이동하게 해준다.
+def profile(request):
+    return render(request, 'accounts/profile.html')
