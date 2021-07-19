55fcc1323e60bf69f78d9863b639a0ca0560d1d9
Yeram <ramye@naver.com>
2021-07-19 13:23:02 +0900

    중급 04강 로그아웃 구현하기

--- AskdjangoBasic/askdjango/accounts/urls.py
+++ AskdjangoBasic/askdjango/accounts/urls.py
@@ -2,9 +2,10 @@ from django.contrib.auth import views as auth_views
 from django.urls import path
 from . import views
 
-app_name = 'accounts'
+#app_name = 'accounts'
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('profile/', views.profile, name='profile'),
 ]
--- AskdjangoBasic/askdjango/askdjango/settings.py
+++ AskdjangoBasic/askdjango/askdjango/settings.py
@@ -128,8 +128,7 @@ STATIC_URL = '/static/'
 from django.urls import reverse_lazy
 
 LOGIN_URL = reverse_lazy('login')
-
-#LOGIN_REDIRECT_URL = reverse_lazy('profile')
+LOGIN_REDIRECT_URL = reverse_lazy('login')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
