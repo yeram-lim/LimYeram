db42af96b66c49ff08a959aee5d6e37e8261aa68
Yeram <ramye@naver.com>
2021-07-19 15:43:48 +0900

    중급 07강 회원가입과 동시에 로그인하기

--- AskdjangoBasic/askdjango/accounts/views.py
+++ AskdjangoBasic/askdjango/accounts/views.py
@@ -1,9 +1,10 @@
 from django.conf import settings
+from django.contrib.auth import login as auth_login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.views.generic import CreateView
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, resolve_url
 from .forms import SignupForm
 
 # 함수 기반
@@ -12,18 +13,29 @@ from .forms import SignupForm
 #         form = SignupForm(request.POST)
 #         if form.is_valid():
 #             user = form.save()
-#             return redirect(settings.LOGIN_URL)
+#             auth_login(request, user)#로그인 처리
+#             return redirect('profile')
 #     else:
 #         form = SignupForm()
 #     return render(request, 'accounts/signup.html', {
 #         'form': form,
 #     })
 
+
 # 클래스 기반
-signup = CreateView.as_view(model=User, 
-        form_class=SignupForm,
-        success_url=settings.LOGIN_URL,
-        template_name='accounts/signup.html')
+class SignupView(CreateView):
+    model = User
+    form_class = SignupForm
+    template_name = 'accounts/signup.html'
+
+    def get_success_url(self):
+        return resolve_url('profile')
+    def form_valid(self, form):
+        user = form.save()
+        auth_login(self.request, user)
+        return redirect(self.get_success_url())
+
+signup = SignupView.as_view()
 
 @login_required #로그인 안 한 사람은 settings의 login으로 이동하게 해준다.
 def profile(request):
