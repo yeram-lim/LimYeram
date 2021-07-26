09b235ba818a10312483855e6b2fad9a4a2a7161
Yeram <ramye@naver.com>
2021-07-26 19:09:46 +0900

    [TRY] like function

--- piro15_Ajax_Pirostagram/piro/urls.py
+++ piro15_Ajax_Pirostagram/piro/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     path('', view=views.post_list, name='post_list'),
     path('<int:pk>/', view=views.post_detail, name='post_detail'),
     path('create/', view=views.post_create, name='post_create'),
-    path('update/<int:pk>/', view=views.post_update, name='post_update'),
-    path('delete/<int:pk>/', view=views.post_delete, name='post_delete'),
-    path('comment_ajax/', view=views.comment_ajax, name='comment_ajax'),
     path('like_ajax/', view=views.like_ajax, name='like_ajax'),
+    # path('update/<int:pk>/', view=views.post_update, name='post_update'),
+    # path('delete/<int:pk>/', view=views.post_delete, name='post_delete'),
+    # path('comment_ajax/', view=views.comment_ajax, name='comment_ajax'),
 ]
